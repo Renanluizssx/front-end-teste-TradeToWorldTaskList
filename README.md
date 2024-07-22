a8616f91e4b7d2387c6ce48b5ffb07a76e69601f
Rafael Rapizo Nery <rafaelrapnery@gmail.com>
2024-07-22 15:17:05 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Creating components
+
+- Under the `components` folder:
+  - Create the TSX for the component
+  - Create a css file for the component
+
+# Creating custom hooks
+
+- Under the `hooks` folder:
+  - UI Hook? The hook name should be `use{componentName}.hook.ts`
+  - Helper hook? The hook name should follow method restrictions (be a verb)
---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+// TODO: Create a new component called `TaskList` in the `components` folder
+// TODO: Create a new component called `TaskForm` in the `components` folder
+// TODO: Create a new component called `TaskListItem` in the `components` folder
+
+// TODO: A user should be able to see a list of tasks
+// TODO: A user should be able to add a new task to the list (with a title)
+// TODO: A user should be able to delete a task from the list
+// TODO: A user should be able to mark a task as complete
+// TODO: A user should be able to distinguish between completed and incomplete tasks
+// TODO: A user should be able to count the total number of tasks
+// TODO: A user should be able to count the number of completed tasks
+// TODO: A user should be able to count the number of incomplete tasks
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">trade to world task list</h1>
+      {/* Add components here... */}
+    </div>
+  );
+}
+
+export default App;
